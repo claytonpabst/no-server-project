8e2d98b6d237e3d9196475f9f4804d20d97a337e
Clayton Pabst <claytonpabst@gmail.com>
2017-07-15 14:22:15 -0600

    giphy axios call

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import ReactAudioPlayer from 'react-audio-player';
 import {Link} from 'react-router-dom'
 import MusicPlayer from './../MusicPlayer/MusicPlayer'
+import axios from 'axios';
 
 import './Artists.css';
 
@@ -15,6 +16,13 @@ export default class Artists extends Component {
 
   }
 
+  componentDidMount () {
+    var test = 'Brad Pitt'
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${ test }&api_key=d4ed0114c75c4e4e961030332aaf000a&limit=5`)
+    .then( (response) => {
+      console.log(response)
+    })
+  }
 
 
   render() {
@@ -23,6 +31,7 @@ export default class Artists extends Component {
 
     return (
       <div className="">
+        {/*<iframe src={ }></iframe>*/}
 
         <div>
             <Link to='/artistprofile/cleightyp'>Cleighty_P</Link>
